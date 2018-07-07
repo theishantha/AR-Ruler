a60cb5f2f452b5a9000347d1ed022d1cd990f6a6
Ishantha Udara <ishanthaudara96@gmail.com>
2018-07-07 17:58:40 +0530

    3D Pythagorean Theorem added

--- AR Ruler/ViewController.swift
+++ AR Ruler/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     @IBOutlet var sceneView: ARSCNView!
     
+    var dotNodes = [SCNNode]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -69,8 +71,33 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             dotNode.position = SCNVector3(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y, hitResult.worldTransform.columns.3.z)
             
             sceneView.scene.rootNode.addChildNode(dotNode)
-            
+     
+          dotNodes.append(dotNode)
+     
+     if dotNodes.count >= 2 {
+            calculate()
         }
-        
+     
+}
+     func calculate(){
+          let start = dotNodes[0]
+          let end = dotNodes[1]
+          
+          print(start.position)
+          print(end.position)
+          
+          
+          let distance = sqrt(pow(end.position.x - start.position.x, 2) + pow(end.position.y - start.position.y, 2) + pow(end.position.z - start.position.z, 2))
+          
+          
+          print(abs(distance))
+          
+          
+//          distance = √ ((x2-x1)^2 + (y2-y1)^2 + (z2-z1)^2)
+          
+     }
+          
+     
         
 }
+
