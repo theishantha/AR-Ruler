bdffc3d6358c8d85aa93e20aad8201fb4628074b
Ishantha Udara <ishanthaudara96@gmail.com>
2018-07-07 16:39:13 +0530

    cont updated

--- AR Ruler/ViewController.swift
+++ AR Ruler/ViewController.swift
@@ -12,6 +12,7 @@ import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -22,15 +23,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         
-//        // Show statistics such as fps and timing information
-//        sceneView.showsStatistics = true
-//
-//        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-//
-//        // Set the scene to the view
-//        sceneView.scene = scene
     }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -42,12 +36,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.run(configuration)
     }
     
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
         // Pause the view's session
         sceneView.session.pause()
     }
+    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touchLocation = touches.first?.location(in: sceneView){
@@ -59,53 +55,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
     }
         
-    func addDot(at hitResult : ARHitTestResult ){
     
-        let dotGeometry = SCNSphere(radius: 0.005)
-        let material = SCNMaterial()
-        material.diffuse.contents = UIColor.red
+    func addDot(at hitResult : ARHitTestResult ){
         
-        dotGeometry.materials = [material]
+            let dotGeometry = SCNSphere(radius: 0.005)
+            let material = SCNMaterial()
+            material.diffuse.contents = UIColor.red
+            
+            dotGeometry.materials = [material]
+            
+            let dotNode = SCNNode(geometry: dotGeometry)
+            
+            dotNode.position = SCNVector3(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y, hitResult.worldTransform.columns.3.z)
+            
+            sceneView.scene.rootNode.addChildNode(dotNode)
+            
+        }
         
-        let dotNode = SCNNode(geometry: dotGeometry)
         
-        dotNode.position = SCNVector3(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y, hitResult.worldTransform.columns.3.z)
-        
-        sceneView.scene.rootNode.addChildNode(dotNode)
-        
-    }
-        
-        
-    }
-//
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Release any cached data, images, etc that aren't in use.
-//    }
-//
-//    // MARK: - ARSCNViewDelegate
-//
-///*
-//    // Override to create and configure nodes for anchors added to the view's session.
-//    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-//        let node = SCNNode()
-//
-//        return node
-//    }
-//*/
-//
-//    func session(_ session: ARSession, didFailWithError error: Error) {
-//        // Present an error message to the user
-//
-//    }
-//
-//    func sessionWasInterrupted(_ session: ARSession) {
-//        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-//
-//    }
-//
-//    func sessionInterruptionEnded(_ session: ARSession) {
-//        // Reset tracking and/or remove existing anchors if consistent tracking is required
-//
-//    }
-
+}
